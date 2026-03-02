ba8b1122d9f52e3b50269740c0f70a9050505b2d
havando1302 <hado5201314@gmail.com>
2026-03-02 22:42:23 +0700

    Update dữ liệu màu cho ứng dụng

---
+++ lib/constants/app_colors.dart
@@ -0,0 +1,20 @@
+class MyColor {
+  static const int white = 0xFFFFFFFF;
+  static const int black = 0xFF121212;
+
+  static const int pr1 = 0xFF0F1F17; // Xanh đen rất đậm (background)
+  static const int pr2 = 0xFF1DB954; // Xanh lá  (main)
+  static const int pr3 = 0xFF1E1E1E; // Nền card
+  static const int pr4 = 0xFF1DB954; // Xanh lá nhấn
+  static const int pr5 = 0xFF14833B; // Xanh lá đậm
+  static const int pr6 = 0xFF0A3D1E; // Xanh lá tối
+
+  static const int se1 = 0xFF121212; // Nền chính
+  static const int se2 = 0xFF181818; // Card / section
+  static const int se3 = 0xFF282828; // Card hover
+  static const int se4 = 0xFFB3B3B3; // Text phụ
+  static const int se5 = 0xFFFFFFFF; // Text chính
+
+  static const int grey = 0xFFB3B3B3; // Xám
+  static const int red = 0xFFE53935;
+}
