2962c0513737cdb35d7d1a59b304744515f7d5d1
havando1302 <hado5201314@gmail.com>
2026-03-03 11:28:36 +0700

    Nhập dữ liệu login

--- lib/views/login/login.dart
+++ lib/views/login/login.dart
@@ -70,6 +70,66 @@ class _LoginPageState extends State<LoginPage> {
                         ),
                       ),
                     ),
+                    const SizedBox(height: 6),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "name@gmail.com",
+                        filled: true,
+                        fillColor: Colors.white,
+                        contentPadding: const EdgeInsets.symmetric(
+                          horizontal: 16,
+                          vertical: 14,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 16),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        "Mật Khẩu",
+                        style: const TextStyle(
+                          color: Color(MyColor.white),
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 6),
+                    TextField(
+                      obscureText: obscurePassword,
+                      decoration: InputDecoration(
+                        hintText: "Nhập mật khẩu",
+                        filled: true,
+                        fillColor: Colors.white,
+                        suffixIcon: IconButton(
+                          icon: Icon(
+                            obscurePassword
+                                ? Icons.visibility_off
+                                : Icons.visibility,
+                          ),
+                          onPressed: () {
+                            setState(() {
+                              obscurePassword = !obscurePassword;
+                            });
+                          },
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: Text(
+                          "Quên mật khẩu?",
+                          style: TextStyle(color: Color(MyColor.pr4)),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               ),
