887ceb7c9b50dbcdb83434c6fb367af7cd710703
havando1302 <hado5201314@gmail.com>
2026-03-04 15:50:41 +0700

    Xây dựng logic lọc và nền cho trang home

--- lib/views/home/app_home.dart
+++ lib/views/home/app_home.dart
@@ -1,13 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:musicbox/models/songs_model.dart';
+import 'package:musicbox/models/albums_model.dart';
+import 'package:musicbox/constants/app_colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String selectedCountry = 'Tất cả';
+
+  List<SongModel> get filteredSongs {
+    if (selectedCountry == 'Tất cả') {
+      return SongModel.mockSongs;
+    } else if (selectedCountry == 'Việt Nam') {
+      return SongModel.mockSongs
+          .where((song) => song.country == 'Việt Nam')
+          .toList();
+    } else {
+      return SongModel.mockSongs
+          .where((song) => song.country != 'Việt Nam')
+          .toList();
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final topSongs = SongModel.mockSongs.take(5).toList();
+
     return Scaffold(
-      appBar: AppBar(title: const Text('Trang chủ'), centerTitle: true),
-      body: const Center(child: Text('HOME PAGE')),
+      backgroundColor: Color(MyColor.pr1),
+      body: ListView(
+        padding: const EdgeInsets.all(16),
+        children: [
+          // Những album nổi bật.
+          const Text(
+            'Tuyển tập',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.bold,
+              color: Color(MyColor.white),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
