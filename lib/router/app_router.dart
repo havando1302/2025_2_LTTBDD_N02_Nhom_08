ecc19905b5064158e4aee3d29e6c93e452526ee8
havando1302 <hado5201314@gmail.com>
2026-03-04 14:13:44 +0700

    Kết nối router chuyển trang

--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:musicbox/views/login/login.dart';
+import 'package:musicbox/views/home/navbar_home.dart';
 
 class RouterCustom {
   static final GoRouter router = GoRouter(
@@ -11,6 +12,13 @@ class RouterCustom {
         name: 'login',
         builder: (BuildContext context, GoRouterState state) {
           return const LoginPage();
+        },
+      ),
+      GoRoute(
+        path: '/home',
+        name: 'home',
+        builder: (BuildContext context, GoRouterState state) {
+          return const HomeCenter();
         },
       ),
     ],
