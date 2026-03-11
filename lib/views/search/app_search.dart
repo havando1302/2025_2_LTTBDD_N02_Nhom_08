dedd1cd7a52ac19f2fed7e51bee6c470d026c5a5
X1212 <nguyenquangtuan@MacBook-Air-M1.local>
2026-03-11 12:10:18 +0700

    thêm thanh tìm kiếm

--- lib/views/search/app_search.dart
+++ lib/views/search/app_search.dart
@@ -9,6 +9,8 @@ class PageSearch extends StatefulWidget {
 }
 
 class _PageSearchState extends State<PageSearch> {
+  final TextEditingController searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,7 +18,38 @@ class _PageSearchState extends State<PageSearch> {
       body: SafeArea(
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          children: [],
+          children: [_searchBar()],
+        ),
+      ),
+    );
+  }
+
+  Widget _searchBar() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 15),
+        height: 50,
+        decoration: BoxDecoration(
+          color: Color(MyColor.grey).withOpacity(0.3),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Row(
+          children: [
+            const Icon(Icons.search, color: Color(MyColor.white)),
+            const SizedBox(width: 10),
+            Expanded(
+              child: TextField(
+                controller: searchController,
+                decoration: const InputDecoration(
+                  hintText: "Tìm kiếm bài hát, nghệ sĩ, album",
+                  hintStyle: TextStyle(color: Colors.white70),
+                  border: InputBorder.none,
+                ),
+                style: const TextStyle(color: Colors.white),
+              ),
+            ),
+          ],
         ),
       ),
     );
